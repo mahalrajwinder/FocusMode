5b4580ba1cee32e5c201b536d8dca8900d68e422
Rajwinder Singh <44937101+mahalrajwinder@users.noreply.github.com>
2020-01-26 14:09:21 -0800

    Initial Commit

---
+++ FocusMode/Controllers/WelcomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WelcomeViewController.swift
+//  FocusMode
+//
+//  Copyright © 2020 Rajwinder Singh. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
